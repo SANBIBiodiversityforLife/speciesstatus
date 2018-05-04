6b0fbdb7e8a81727434943ff5eed613a63a31d80
Dewidine Van Der Colff <VanDerColffD@sanbi.local>
2018-05-04 14:40:40 +0200

    templates/website/about.html         modified:   templates/website/partners.html

---
+++ db/sql_query1.sql
@@ -0,0 +1,9 @@
+﻿SELECT taxon.name, assess.id, bib.*
+FROM taxa_taxon taxon
+JOIN redlist_assessment assess
+ON taxon.id = assess.taxon_id
+JOIN redlist_assessment_references ref ON 
+assess.id = ref.assessment_id
+JOIN biblio_reference bib ON
+bib.id = ref.reference_id
+WHERE taxon.name ILIKE 'xen%'
